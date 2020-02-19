93195abf8c3a878f9a9171b08a5b1953ed4961ab
Adam Bien <abien@adam-bien.com>
2020-02-19 10:52:21 +0100

    40_creating_a_store

--- events/src/app.js
+++ events/src/app.js
@@ -1,4 +1,24 @@
-console.log("Hello Web Standards");
+import { createStore } from "./lib/redux.js";
 
+const multiply = (state = 21, action) => { 
+    console.log(state, action);
+    if(action.type==='M')
+        return state * action.payload;
+    return state;
+}
+
+const store = createStore(multiply);
+
+store.subscribe(_ => console.log('state changed',store.getState()));
+
+const initial = store.getState();
+console.log('initial', initial);
+
+store.dispatch({
+    type: 'M',
+    payload: 2
+})
+
+console.log(store.getState());
 
 
