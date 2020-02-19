caed19c244f3424ed0996cf0182e05271e4c0c61
Adam Bien <abien@adam-bien.com>
2020-02-19 11:07:12 +0100

    50_redux_dev_tools

--- events/src/app.js
+++ events/src/app.js
@@ -7,7 +7,7 @@ const multiply = (state = 21, action) => {
     return state;
 }
 
-const store = createStore(multiply);
+const store = createStore(multiply,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 store.subscribe(_ => console.log('state changed',store.getState()));
 
@@ -17,6 +17,10 @@ console.log('initial', initial);
 store.dispatch({
     type: 'M',
     payload: 2
+})
+store.dispatch({
+    type: 'M',
+    payload: 3
 })
 
 console.log(store.getState());
