729e7a13da1c3fa928d0c1106999eabb30a1702a
Adam Bien <abien@adam-bien.com>
2020-02-27 11:46:45 +0100

    90_inputs_and_change_event

--- events/src/creation/boundary/NewEvent.js
+++ events/src/creation/boundary/NewEvent.js
@@ -1,14 +1,25 @@
 import { render,html } from "../../lib/lit-html.js";
 class NewEvent extends HTMLElement{ 
+
+    constructor() { 
+        super();
+        this.event = {};
+    }
     connectedCallback() { 
         const template = html`
-            <input placeholder="name">
+            <input name="eventname" @change=${e=>this.onUserInput(e)} placeholder="eventname">
+            <input name="description" @change=${e=>this.onUserInput(e)} placeholder="description">
             <button @click=${_ => this.newEvent()}>create</button>
         `;
         render(template,this);
     }
     newEvent() { 
-        console.log('------ button clicked');
+        console.log('------ saving',this.event);
+    }
+    onUserInput({ target: { name,value } }) { 
+        console.log(name, value);
+        this.event[name] = value;
     }
 }
+
 customElements.define('a-newevent',NewEvent);
