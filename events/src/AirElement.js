fef7d89274d87d14cde769126128601be20e898d
Adam Bien <abien@adam-bien.com>
2020-02-28 11:17:26 +0100

    140_adding_events_to_redux_store

--- events/src/AirElement.js
+++ events/src/AirElement.js
@@ -1,11 +1,18 @@
 import { render } from "./lib/lit-html.js";
 import { createStore } from "./lib/redux.js";
 
-const events = (state = {}, action) => { 
+const events = (state = { events: [] }, action) => { 
+    const { type, payload } = action;
+    switch (type) { 
+        case 'NEW_EVENT_CREATED':
+            return {
+                events: state.events.concat(payload)
+            }    
+    }
     console.log(state,action);
     return state;
 }
-const store = createStore(events);
+const store = createStore(events,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default class AirElement extends HTMLElement { 
     connectedCallback() { 
