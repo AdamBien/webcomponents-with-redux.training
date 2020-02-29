b7f5b874b59b70190247ead627bbc391200cc877
Adam Bien <abien@adam-bien.com>
2020-02-29 13:59:37 +0100

    160_rendering_events_in_a_list

--- events/src/overview/boundary/EventsOverview.js
+++ events/src/overview/boundary/EventsOverview.js
@@ -3,9 +3,14 @@ import AirElement from "../../AirElement.js";
 class EventsOverview extends AirElement { 
 
     view() { 
+        const eventList = this.state.events;
         console.log('UPDATING +1',this.state);
         return html`
-        <h2>hello,overview</h2>
+        <ol>
+         ${eventList.map(({ name, description }) => html`
+            <li>${name} => ${description}</li>
+         `)}
+        </ol>
         `;
     }
 
