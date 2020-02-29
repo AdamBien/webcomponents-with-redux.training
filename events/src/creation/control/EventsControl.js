0e259beb5703534cedcbb7d72f54f1895f7e4dd7
Adam Bien <abien@adam-bien.com>
2020-02-29 14:05:58 +0100

    170_copies_not_references

--- events/src/creation/control/EventsControl.js
+++ events/src/creation/control/EventsControl.js
@@ -1,6 +1,7 @@
 import { store } from "../../AirElement.js";
 
-const createEvent = payload => { 
+const createEvent = event => { 
+    const payload = Object.assign({},event);
     store.dispatch({
         type: 'NEW_EVENT_CREATED',
         payload
