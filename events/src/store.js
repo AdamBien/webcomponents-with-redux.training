24693a097e8d213e1580edebecc4599e99e1a8ac
Adam Bien <abien@adam-bien.com>
2020-03-06 11:00:31 +0100

    320_combining_reducers

--- events/src/store.js
+++ events/src/store.js
@@ -1,9 +1,12 @@
-import { createStore } from "./lib/redux.js";
+import { createStore,combineReducers } from "./lib/redux.js";
 import events from "./creation/entity/EventsReducer.js";
+import filter from "./filter/entity/FilterReducer.js";
 const deepCopy = input => JSON.parse(JSON.stringify(input));
 
+const combinedReducer = combineReducers({events,filter});
+
 const copyingEvent = (state, action) => { 
-    return deepCopy(events(state,action));
+    return deepCopy(combinedReducer(state,action));
 }
 
 const store = createStore(copyingEvent,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
