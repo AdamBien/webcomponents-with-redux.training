86856e2d7e90d2192011ed198f524874d41f3c6e
Adam Bien <abien@adam-bien.com>
2020-03-07 15:24:28 +0100

    370_testing_empty_function

--- events/test/test.js
+++ events/test/test.js
@@ -1,12 +1,10 @@
+import matchesCriteria from "./overview/entity/Filter.js";
 mocha.setup('bdd');
 const assert = chai.assert;
 
-describe('#hello', function () { 
-    it('world', function () { 
-        assert.isTrue(true);
-    })
-    it('...', function () { 
-        assert.isTrue(false);
+describe('#overview.entity.Filter', function () { 
+    it('matchesCriteria_true_emptyEvent', function () { 
+        assert.isTrue(matchesCriteria({},''));
     })
 })
 
