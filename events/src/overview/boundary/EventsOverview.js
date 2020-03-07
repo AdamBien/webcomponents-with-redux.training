f71910dfbef2bca616c2d28b789a7e56d211cf4f
Adam Bien <abien@adam-bien.com>
2020-03-07 16:25:04 +0100

    390_integrating_filter_functionality

--- events/src/overview/boundary/EventsOverview.js
+++ events/src/overview/boundary/EventsOverview.js
@@ -6,12 +6,12 @@ import matchesCriteria from '../entity/Filter.js';
 class EventsOverview extends AirElement { 
 
     view() { 
-        const { events: { events:eventList },filter } = this.state;
+        const { events: { events: eventList }, filter: { filter } } = this.state;
         console.log('..EO.',filter);
         return html`
         <a-events-filter></a-events-filter>
         <ol>
-         ${eventList.filter(e => matchesCriteria(e)).map(({ eventname, description }) => html`
+         ${eventList.filter(e => matchesCriteria(e,filter)).map(({ eventname, description }) => html`
             <li>${eventname} => ${description}</li>
          `)}
         </ol>
