79c09035676b857c871ad0785f33f2bef1b00d58
Adam Bien <abien@adam-bien.com>
2020-03-17 12:29:22 +0100

    700_fetch_error_handling

--- events/src/creation/control/LinkValidatorControl.js
+++ events/src/creation/control/LinkValidatorControl.js
@@ -1,9 +1,15 @@
 export const validate = async (url) => { 
-    const response = await fetch('http://localhost:8080/validations', {
-        method: 'POST',
-        body: url
-    });
-    const result = await response.json();
+    let response, result;
+    try {
+        response = await fetch('http://localhost:8080/validations', {
+            method: 'POST',
+            body: url
+        });
+        result = await response.json();
+    } catch (error) { 
+        console.log('server not available', error);
+        return;
+    }
     const { ok, status } = result;
     console.log(`URL is valid? ${ok} with status ${status} `);
 }
