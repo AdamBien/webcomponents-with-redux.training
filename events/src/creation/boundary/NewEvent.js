bcaf19ee7a53c760988d285be46a50267365a04c
Adam Bien <abien@adam-bien.com>
2020-04-01 13:22:30 +0200

    960_receiving_date_picker_changes

--- events/src/creation/boundary/NewEvent.js
+++ events/src/creation/boundary/NewEvent.js
@@ -26,8 +26,7 @@ class NewEvent extends AirElement{
         return html`
         <form>
             ${this.input({name:'eventname'})}
-            ${this.input({ name: 'startdate', type: "date" })}
-            <a-dateinput name='startdate' ?disabled=${status}></a-dateinput>
+            <a-dateinput name='startdate' ?disabled=${status} @change=${e => this.onUserInput(e)}></a-dateinput>
             ${this.input({ name: 'enddate', type: 'date' })}
             ${this.input({ name: 'link',type:'url'})}
             ${this.input({name:'description'})}
