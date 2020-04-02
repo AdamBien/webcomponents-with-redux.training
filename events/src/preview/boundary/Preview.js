4adc63b61fce9dcfd4efd2715d0041e124c39008
Adam Bien <abien@adam-bien.com>
2020-04-02 12:34:04 +0200

    1030_preview_the_eventname

--- events/src/preview/boundary/Preview.js
+++ events/src/preview/boundary/Preview.js
@@ -2,9 +2,14 @@ import AirElement from "../../AirElement.js";
 import { html } from "../../lib/lit-html.js";
 
 class Preview extends AirElement { 
+
+    extractState({ events: { form} }) { 
+        return { form };
+    }
+
     view() { 
         return html`
-            <h2>preview</h2>
+            <h2>${this.state.form.eventname}</h2>
         `;
     }
 }
