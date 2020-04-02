f40bff60048896bd00137fe620a673e8b3e70eb0
Adam Bien <abien@adam-bien.com>
2020-04-02 14:43:27 +0200

    1070_programmatic_routing

--- events/src/overview/control/EventsControl.js
+++ events/src/overview/control/EventsControl.js
@@ -1,5 +1,6 @@
 import { EVENT_SELECTED,EDIT_SELECTED,DELETE_SELECTED_EVENTS } from "../entity/OverviewReducer.js";
 import store from "../../store.js";
+import {Router} from '../../lib/@vaadin/router.js';
 export const eventSelected = (name, checked) => { 
     store.dispatch({
         type: EVENT_SELECTED,
@@ -14,6 +15,12 @@ export const editSelected = () => {
     store.dispatch({
         type: EDIT_SELECTED
     });
+}
+
+export const previewSelected = () => {
+    editSelected();
+    Router.go("/preview");
+
 }
 export const deleteSelected = () => { 
     store.dispatch({
