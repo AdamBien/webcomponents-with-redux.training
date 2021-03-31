46b01e73f26d72da31d0093d0117668042d0608d
Adam Bien <abien@adam-bien.com>
2021-03-31 10:53:04 +0200

    3040_postinstallation_window_process

--- events/src/filter/control/FilterControl.js
+++ events/src/filter/control/FilterControl.js
@@ -1,5 +1,6 @@
 import store from '../../store.js'
 import { FILTER_CHANGED } from "../entity/FilterReducer.js";
+import {  } from "../../lib/redux-toolkit.esm.js";
 const onFilterChanged = payload => { 
 
     store.dispatch({
