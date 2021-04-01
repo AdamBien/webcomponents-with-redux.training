26243b87ee600f250de522261ffd9af66bcc25c0
Adam Bien <abien@adam-bien.com>
2021-04-01 21:08:32 +0200

    3070_event_selected_action_migration

--- events/src/overview/control/EventsControl.js
+++ events/src/overview/control/EventsControl.js
@@ -1,14 +1,15 @@
 import { EVENT_SELECTED,EDIT_SELECTED,DELETE_SELECTED_EVENTS } from "../entity/OverviewReducer.js";
 import store from "../../store.js";
-import {Router} from '../../lib/@vaadin/router.js';
-export const eventSelected = (name, checked) => { 
-    store.dispatch({
-        type: EVENT_SELECTED,
-        payload: {
-            name,
-            checked
-        }
+import { Router } from '../../lib/@vaadin/router.js';
+import { createAction } from "../../lib/redux-toolkit.esm.js";
+export const eventSelectedAction =  createAction(EVENT_SELECTED);
+
+export const eventSelected = (name, checked) => {
+    const action = eventSelectedAction( {
+        name,
+        checked
     });
+    store.dispatch(action);
 
 }
 export const editSelected = () => {
