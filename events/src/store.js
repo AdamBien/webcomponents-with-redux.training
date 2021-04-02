4eed41d4d22df217acde60851391bc221549f402
Adam Bien <abien@adam-bien.com>
2021-04-02 15:40:07 +0200

    3200_easy_initialization_with_configureStore

--- events/src/store.js
+++ events/src/store.js
@@ -1,25 +1,23 @@
-import { createStore,combineReducers } from "./lib/redux.js";
 import { events } from "./creation/entity/EventsReducer.js";
 import { overview } from "./overview/entity/OverviewReducer.js";
 import { filter } from "./filter/entity/FilterReducer.js";
 import { status } from "./status/entity/StatusReducer.js";
 import { load } from "./localstorage/control/StorageControl.js";
+import { configureStore } from "./lib/redux-toolkit.esm.js";
 
 const chainedEventsReducer = (state, action) => { 
     const eventsResult = events(state, action);
     return overview(eventsResult,action);
 }
 
-const combinedReducer = combineReducers({
+const reducer = {
     events:chainedEventsReducer,
     filter,
     status
-});
+};
 
 
-
-let initialState = load();
-
-
-const store = createStore(combinedReducer,initialState,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const preloadedState = load();
+const config = {reducer,preloadedState};
+const store = configureStore(config);
 export default store;
