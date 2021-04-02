8fb9071f0e9cd3a983b08fa1adb281725edfde39
Adam Bien <abien@adam-bien.com>
2021-04-02 06:12:04 +0200

    3120_status_control_migration

--- events/src/status/control/StatusControl.js
+++ events/src/status/control/StatusControl.js
@@ -1,30 +1,22 @@
+import { createAction } from "../../lib/redux-toolkit.esm.js";
 import store from "../../store.js";
 import { ERROR_HAPPENED,MESSAGE_CLEARED,REQUEST_STARTED,REQUEST_COMPLETED } from "../entity/StatusReducer.js";
-
+export const clearMessageAction = createAction(MESSAGE_CLEARED);
 export const clearMessage = _ => { 
-    store.dispatch({
-        type: MESSAGE_CLEARED
-    });
+    store.dispatch(clearMessageAction());
 }
+export const errorHappenedAction = createAction(ERROR_HAPPENED);
 export const errorHappened = (error='unknown',message) => { 
-    store.dispatch({
-        type: ERROR_HAPPENED,
-        payload: {
-            error,
-            message
-        }
-    });
+    store.dispatch(errorHappenedAction({
+        error,
+        message
+    }));
 }
-
+export const requestStartedAction = createAction(REQUEST_STARTED);
 export const requestStarted = (payload) => { 
-    store.dispatch({
-        type: REQUEST_STARTED,
-        payload
-    });
+    store.dispatch(requestStartedAction(payload));
 }
+export const requestCompletedAction = createAction(REQUEST_COMPLETED);
 export const requestCompleted = (payload) => { 
-    store.dispatch({
-        type: REQUEST_COMPLETED,
-        payload
-    });
+    store.dispatch(requestCompletedAction(payload));
 }
