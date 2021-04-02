dec413867c7549015b249c7410b98bdaa1d2daf2
Adam Bien <abien@adam-bien.com>
2021-04-02 06:29:34 +0200

    3140_removing_optional_constants_from_status_control

--- events/src/status/control/StatusControl.js
+++ events/src/status/control/StatusControl.js
@@ -1,28 +1,22 @@
 import { createAction } from "../../lib/redux-toolkit.esm.js";
 import store from "../../store.js";
 
-const ERROR_HAPPENED = 'ERROR_HAPPENED';
-const MESSAGE_CLEARED = 'MESSAGE_CLEARED';
-const REQUEST_STARTED = 'REQUEST_STARTED';
-const REQUEST_COMPLETED = 'REQUEST_COMPLETED';
-
-
-export const clearMessageAction = createAction(MESSAGE_CLEARED);
+export const clearMessageAction = createAction("clearMessageAction");
 export const clearMessage = _ => { 
     store.dispatch(clearMessageAction());
 }
-export const errorHappenedAction = createAction(ERROR_HAPPENED);
+export const errorHappenedAction = createAction("errorHappenedAction");
 export const errorHappened = (error='unknown',message) => { 
     store.dispatch(errorHappenedAction({
         error,
         message
     }));
 }
-export const requestStartedAction = createAction(REQUEST_STARTED);
+export const requestStartedAction = createAction("requestStartedAction");
 export const requestStarted = (payload) => { 
     store.dispatch(requestStartedAction(payload));
 }
-export const requestCompletedAction = createAction(REQUEST_COMPLETED);
+export const requestCompletedAction = createAction("requestCompletedAction");
 export const requestCompleted = (payload) => { 
     store.dispatch(requestCompletedAction(payload));
 }
