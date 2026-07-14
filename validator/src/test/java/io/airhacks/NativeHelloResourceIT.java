fa424a1d756902c43cf09d0022d61e9d66e5ebac
Adam Bien <abien@adam-bien.com>
2026-07-14 08:16:20 +0200

    superfluous tests removed

--- validator/src/test/java/io/airhacks/NativeHelloResourceIT.java
+++
@@ -1,9 +0,0 @@
-package io.airhacks;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeHelloResourceIT extends HelloResourceTest {
-
-    // Execute the same tests but in native mode.
-}
